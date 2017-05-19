bc888ce86ef88575867f2da68cadeafa6e2fada0
Julia Belyakova <julbinb@gmail.com>
2017-05-19 11:54:52 -0400

    GenericModuleLib | Implementation (Implmn1) static checking completed

--- GenericModuleLib/MImplmn1.v
+++ GenericModuleLib/MImplmn1.v
@@ -129,18 +129,21 @@ Module MImplmn1Defs
       (ok_prop', cl')
     end end.
 
+  (** [initP] could be any provable proposition.
+      This is more convenient for doing proofs than 
+        using [True] as initial proposition, for example. *)
   Definition terms_ok' 
-             (c : ctx) (imap : intrfs_map) (cl : ctx_local)
+             (c : ctx) (imap : intrfs_map) (cl : ctx_local) (initP : Prop)
              (defs : list (id * tm)) : Prop * ctx_local :=
     List.fold_left
       (process_dep_member c imap)
       defs
-      (True, cl).
+      (initP, cl).
 
   Definition terms_ok 
              (c : ctx) (imap : intrfs_map) 
              (defs : list (id * tm)) : Prop :=
-    fst (terms_ok' c imap ctxl_init defs).
+    fst (terms_ok' c imap ctxl_init True defs).
 
 
   Definition implmn_ok 
@@ -218,149 +221,189 @@ Module MImplmn1Props
   Module Import MII := MImplmn1Interp MI MIB TOkI.
 
 
-  Lemma process_dep_member_b_preserves_false :
-    forall (c : ctx) (imap : intrfs_map)
-           (ds : list (id * tm)) (cl : ctx_local),
-      fold_left (process_dep_member_b c imap) ds (false, cl) = (false, cl).
-  Proof.
-    intros c imap ds.
-    induction ds as [| [nm t] ds' IHds'];
-      intros cl.
-    - (* ds = nil *)
-      simpl. reflexivity.
-    - (* ds = (nm, t) :: ds' *)
-      simpl. apply IHds'.
-  Qed.
-
-  Lemma terms_ok'_b_cons_true :
-    forall (c : ctx) (imap : intrfs_map)
-           (ds : list (id * tm)) (cl : ctx_local)
-           (nm : id) (t : tm),
-      fst (terms_ok'_b c imap cl ((nm, t) :: ds)) = true ->
-      fst (terms_ok'_b c imap (upd_ctx_local cl c imap nm t) ds) = true.
-  Proof.
-    intros c imap ds.
-    unfold terms_ok'_b. simpl.
-    intros cl nm t H.
-    assert (Hok : is_ok_b c imap cl nm t = true).
-    { destruct (is_ok_b c imap cl nm t) eqn:Heq.
-      reflexivity.
-      destruct ds as [| d ds'].
-      + simpl in H. assumption.
-      + rewrite (process_dep_member_b_preserves_false c imap _ _) in H.
-        simpl in H. inversion H. }
-    rewrite Hok in H. assumption.
-  Qed.
-
-  Lemma terms_ok'_b_true__head_ok :
-    forall (c : ctx) (imap : intrfs_map)
-           (ds : list (id * tm)) (cl : ctx_local)
-           (nm : id) (t : tm),
-      fst (terms_ok'_b c imap cl ((nm, t) :: ds)) = true ->
-      is_ok_b c imap cl nm t = true.
-  Proof.
-    intros c imap ds cl nm t H.
-    unfold terms_ok'_b in H. simpl in H.
-    destruct (is_ok_b c imap cl nm t) eqn:Heq.
-    reflexivity.
-    rewrite (process_dep_member_b_preserves_false c imap _ _) in H.
-    simpl in H. assumption.
-  Qed.
-
-
-
-  Lemma terms_ok'__head_ok :
-    forall (c : ctx) (imap : intrfs_map)
-           (ds : list (id * tm)) (cl : ctx_local)
-           (nm : id) (t : tm),
-      fst (terms_ok' c imap cl ((nm, t) :: ds)) ->
-      is_ok c imap cl nm t.
-  Proof.
-    intros c imap ds cl nm t H.
-    unfold terms_ok' in H. simpl in H.
-    
-  Abort.
-
-  Lemma process_dep_member__any_prop :       
-    forall (c : ctx) (imap : intrfs_map)
-           (defs : list (id * tm)) (cl : ctx_local) (P P' : Prop),
-      P -> P' ->
-      fst (List.fold_left (process_dep_member c imap) defs (P, cl)) ->
-      fst (List.fold_left (process_dep_member c imap) defs (P', cl)).
-  Proof.
-    intros c imap ds.
-    induction ds as [| [nm t] ds' IHds'];
-      intros cl P P' HP HP' H.
-    - (* ds = nil *)
-      simpl. assumption.
-    - (* ds = (nm, t) :: ds' *)
-      simpl in *. 
-      apply IHds' with (P := is_ok c imap cl nm t /\ P).
-  Abort.
-
-    
-
-
-  Lemma terms_ok'_b__sound :
-    forall (c : ctx) (imap : intrfs_map)
-           (defs : list (id * tm)) (cl : ctx_local),
-      fst (terms_ok'_b c imap cl defs) = true ->
-      fst (terms_ok' c imap cl defs).
-  Proof.
-    intros c imap ds.
-    induction ds as [| [nm t] ds' IHds'];
-      intros cl H.
-    - constructor.
-    - (* ds = (nm, t) :: ds' *)
-      assert (H' := H).
-      apply terms_ok'_b_true__head_ok in H'.
-      unfold terms_ok'. simpl.
-      apply TOkP.is_ok_b__sound in H'.
-      assert (HTrue : is_ok c imap cl nm t /\ True <-> True).
-      { split; intros HT.
-        constructor.
-        split; assumption. }
-      unfold terms_ok' in IHds'.
-(*      rewrite -> HTrue.
-      
-      apply IHds'.
-      
-      unfold terms_ok'_b in H.
-      simpl in H.
-      specialize (IHds' 
-
-      apply terms_ok'_b_cons_true in H.
-  
-      unfold terms_ok'. simpl.
-      destruct d as [nm tm].
-*)
-  Abort.
-
-  Lemma terms_ok_b__sound :
-    forall (c : ctx) (imap : intrfs_map) (defs : list (id * tm)),
-      terms_ok_b c imap defs = true ->
-      terms_ok c imap defs.
-  Proof.
-    intros c imap ds.
-    induction ds as [| d ds' IHds'];
-      intros H.
-    - constructor.
-    - (* ds = d :: ds' *)
-      unfold terms_ok.
-
-  Abort.
-
-
-
-
 (* ----------------------------------------------------------------- *)
 (** *** Helper Props  *)
 (* ----------------------------------------------------------------- *)
 
   Module Helper.
 
+    (** Soundness *)   
+
+    Lemma process_dep_member_b_preserves_false :
+      forall (c : ctx) (imap : intrfs_map)
+             (ds : list (id * tm)) (cl : ctx_local),
+        fold_left (process_dep_member_b c imap) ds (false, cl) = (false, cl).
+    Proof.
+      intros c imap ds.
+      induction ds as [| [nm t] ds' IHds'];
+        intros cl.
+      - (* ds = nil *)
+        simpl. reflexivity.
+      - (* ds = (nm, t) :: ds' *)
+        simpl. apply IHds'.
+    Qed.
+
+    Lemma terms_ok'_b_cons_true :
+      forall (c : ctx) (imap : intrfs_map)
+             (ds : list (id * tm)) (cl : ctx_local)
+             (nm : id) (t : tm),
+        fst (terms_ok'_b c imap cl ((nm, t) :: ds)) = true ->
+        fst (terms_ok'_b c imap (upd_ctx_local cl c imap nm t) ds) = true.
+    Proof.
+      intros c imap ds.
+      unfold terms_ok'_b. simpl.
+      intros cl nm t H.
+      assert (Hok : is_ok_b c imap cl nm t = true).
+      { destruct (is_ok_b c imap cl nm t) eqn:Heq.
+        reflexivity.
+        destruct ds as [| d ds'].
+        + simpl in H. assumption.
+        + rewrite (process_dep_member_b_preserves_false c imap _ _) in H.
+          simpl in H. inversion H. }
+      rewrite Hok in H. assumption.
+    Qed.
+
+    Lemma terms_ok'_b_true__head_ok :
+      forall (c : ctx) (imap : intrfs_map)
+             (ds : list (id * tm)) (cl : ctx_local)
+             (nm : id) (t : tm),
+        fst (terms_ok'_b c imap cl ((nm, t) :: ds)) = true ->
+        is_ok_b c imap cl nm t = true.
+    Proof.
+      intros c imap ds cl nm t H.
+      unfold terms_ok'_b in H. simpl in H.
+      destruct (is_ok_b c imap cl nm t) eqn:Heq.
+      reflexivity.
+      rewrite (process_dep_member_b_preserves_false c imap _ _) in H.
+      simpl in H. assumption.
+    Qed. 
+
+    Lemma terms_ok'_b__sound :
+      forall (c : ctx) (imap : intrfs_map)
+             (defs : list (id * tm)) (cl : ctx_local) (initP : Prop),
+        initP ->
+        fst (terms_ok'_b c imap cl defs) = true ->
+        fst (terms_ok' c imap cl initP defs).
+    Proof.
+      intros c imap ds.
+      induction ds as [| [nm t] ds' IHds'];
+        intros cl P HP H.
+      - simpl. assumption.
+      - (* ds = (nm, t) :: ds' *)
+        assert (H' := H).
+        apply terms_ok'_b_true__head_ok in H'.
+        assert (H'' := H').
+        apply TOkP.is_ok_b__sound in H''.
+        unfold terms_ok'. simpl.
+        apply IHds'.
+        tauto.
+        unfold terms_ok'_b in H. simpl in H.
+        unfold terms_ok'_b. rewrite H' in H. 
+        assumption.
+    Qed.
+
+(* ----------------------------------------------------------------- *) 
+
+    (** Completeness *)
+
+    Lemma terms_ok'__fst_prop :
+      forall (c : ctx) (imap : intrfs_map)
+             (ds : list (id * tm)) (cl : ctx_local) (initP : Prop)
+             (nm : id) (t : tm),
+      exists Ps : Prop,
+        (fst (terms_ok' c imap cl initP ((nm, t) :: ds)) = Ps)
+        /\ (Ps -> (is_ok c imap cl nm t) /\ initP).
+    Proof.
+      intros c imap ds.
+      induction ds as [| [dnm dt] ds' IHds'];
+        intros cl P nm t.
+      - (* ds = nil *)
+        unfold terms_ok'. simpl. 
+        exists (is_ok c imap cl nm t /\ P).
+        tauto.
+      - (* ds = (dnm, dt) :: ds' *)
+        replace (fst (terms_ok' c imap cl P ((nm, t) :: (dnm, dt) :: ds')))
+        with (fst (terms_ok' c imap (upd_ctx_local cl c imap nm t) 
+                             (is_ok c imap cl nm t /\ P) ((dnm, dt) :: ds')))
+          by (unfold terms_ok'; reflexivity).
+        remember (upd_ctx_local cl c imap nm t) as cl'.      
+        specialize (IHds' cl' (is_ok c imap cl nm t /\ P) dnm dt).
+        inversion IHds' as [Ps HPs].
+        inversion HPs as [Heq Himp].
+        exists Ps. split.
+        assumption. tauto.
+    Qed.
+
+    Lemma terms_ok'__head_ok :
+      forall (c : ctx) (imap : intrfs_map)
+             (ds : list (id * tm)) (cl : ctx_local) (initP : Prop)
+             (nm : id) (t : tm),
+        initP ->
+        fst (terms_ok' c imap cl initP ((nm, t) :: ds)) ->
+        (is_ok c imap cl nm t) /\ initP.
+    Proof.
+      intros c imap ds cl P nm t HP H.
+      pose proof (terms_ok'__fst_prop c imap ds cl P nm t) as Hex.
+      inversion Hex as [Ps [Heq Himp]].
+      rewrite Heq in H. apply Himp in H.
+      assumption.
+    Qed. 
+
+    Lemma terms_ok'_b__complete :
+      forall (c : ctx) (imap : intrfs_map)
+             (defs : list (id * tm)) (cl : ctx_local) (initP : Prop),
+        initP ->
+        fst (terms_ok' c imap cl initP defs) ->
+        fst (terms_ok'_b c imap cl defs) = true.
+    Proof.
+      intros c imap ds.
+      induction ds as [| [nm t] ds' IHds'];
+        intros cl P HP H.
+      - simpl. reflexivity.
+      - (* ds = (nm, t) :: ds' *)
+        assert (H' := H).
+        apply terms_ok'__head_ok in H'.
+        inversion H' as [Hok _].
+        assert (Hok' := Hok). apply TOkP.is_ok_b__complete in Hok'.
+        remember (upd_ctx_local cl c imap nm t) as cl'.
+        replace (terms_ok'_b c imap cl ((nm, t) :: ds'))
+        with (terms_ok'_b c imap cl' ds')
+          by (unfold terms_ok'_b; simpl; subst cl'; rewrite Hok'; reflexivity).
+        apply IHds' with (initP := is_ok c imap cl nm t /\ P).
+        assumption.
+        unfold terms_ok' in *. simpl in *.
+        subst cl'. assumption. assumption.
+    Qed.    
+
   End Helper.
 
+(* ================================================================= *)
+(** *** Properties *)
+(* ================================================================= *)
+
+  Lemma terms_ok_b__sound :
+    forall (c : ctx) (imap : intrfs_map) (defs : list (id * tm)),
+      terms_ok_b c imap defs = true ->
+      terms_ok c imap defs.
+  Proof.
+    intros c imap ds H.
+    unfold terms_ok_b in H.
+    unfold terms_ok.
+    apply Helper.terms_ok'_b__sound with (initP := True) in H.
+    assumption. constructor.
+  Qed.
+
+  Lemma terms_ok_b__complete :
+    forall (c : ctx) (imap : intrfs_map) (defs : list (id * tm)),
+      terms_ok c imap defs -> 
+      terms_ok_b c imap defs = true.
+  Proof.
+    intros c imap ds H.
+    unfold terms_ok in H.
+    unfold terms_ok_b.
+    apply Helper.terms_ok'_b__complete with (initP := True) in H.
+    assumption. constructor.
+  Qed.
 
 End MImplmn1Props.
 
